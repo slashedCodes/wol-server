31da79167538fb24f83b6feb0a9e0a8b751e4703
alexfeed1990 <alexfeed1990real@gmail.com>
2024-05-24 11:50:50 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# WOL-Server
+Basic Wake-On-Lan server with basic authentication.
+
+# Installation
+```
+git clone https://github.com/slashedCodes/wol-server.git
+cd wol-server
+npm i # Install dependencies
+npm i -g nodemon # Install nodemon
+
+# Rename config.json5.example to config.json5
+mv config.json5.example config.json5
+nano config.json5 # Edit config file and adjust to your needs.
+
+# Run using package.json scripts
+npm run start
+
+# Run manually using nodemon
+nodemon index.js
+```
+
+# Usage
+I made this primarily for IOS shortcuts, but it's a simple POST request: <br>
+To wake a computer, make a post request to /wake and include these parameters in a JSON body:
+```
+{
+    "pin": "1234abc", // Pin for authentication. Make sure it's a string.
+    "id": "my-computer" // Computer ID in config.json5
+}
+```
---
+++ index.js
@@ -0,0 +1,124 @@
+// WOL Server, slashedCodes 2024 //
+
+// Imports
+import { wake } from "wol";
+import JSON5 from "json5";
+import * as fs from "fs";
+import * as http from "http";
+import * as url from "url";
+
+// Variables //
+
+const configPath = "./config.json5"; // Only change this if you moved the config.json5 file.
+const config = loadConfig(configPath);
+
+// Functions //
+
+function log(msg) {
+    console.log(`[Info] ${msg}`);
+}
+
+function warning(msg, func) {
+    console.warn(`[Warning] ${func}(): ${msg}`);
+} 
+
+function error(msg, func, quit) {
+    console.error(`[Error] ${func}(): ${msg}`);
+    if(quit) process.exit();
+}
+
+// Load the config file from a specified path.
+function loadConfig(path) {
+    if(fs.existsSync(path)) {
+        const file = fs.readFileSync(path);
+        const config = JSON5.parse(file);
+
+        if (config) {
+            log("Config file loaded successfully.");
+            return config;
+        } else { error("Config file empty or null.", "loadConfig", true); }
+    } else {
+        error("Config file doesn't exist.", "loadConfig", true);
+    }
+} 
+
+function wakeID(id) {
+    return new Promise((resolve) => {
+        const computer = config["computers"][id]
+        if (computer) {
+            const name = computer["name"]
+            const mac = computer["mac"]
+            const ip = computer["ip"]
+            const port = computer["port"]
+
+            log(`Attempting to wake computer ${name} with ID ${id}.`);
+            wake(mac, { address: ip, port: port }, (err, res) => {
+                if (err) {
+                    error(err, "wakeID", false);
+                    resolve(500); // 500 Internal Server Error
+                } else {
+                    log(`Sent magic packet to computer ${name} with ID ${id}!`);
+                    resolve(200); // 200 OK
+                }
+            });
+        } else {
+            warning("ID is invalid.", "wakeID");
+            resolve(400); // 400 Bad request
+        }
+    })
+}
+
+// Web Server Code //
+
+async function getBody(req) {
+    return new Promise((resolve, reject) => {
+        let body = [];
+        
+        req.on("data", chunk => {
+            body.push(chunk);
+        }).on("end", () => {
+            body = Buffer.concat(body).toString();
+
+            try {
+                resolve(JSON.parse(body));
+            } catch (error) {
+                reject(error);
+            }
+        }).on("error", (error) => {
+            reject(error);
+        });
+    });
+}
+
+const server = http.createServer(async (req, res) => {
+    const urlPath = url.parse(req.url).pathname;
+
+    if (req.method === "POST" && urlPath === "/wake") {
+        const data = await getBody(req);
+        if(data["pin"] != config["pin"]) {
+            res.statusCode = 403; // 403 Forbidden
+            res.end("Invalid Pin.");
+        }
+
+        res.statusCode = await wakeID(data["id"]);
+        switch (res.statusCode) {
+            case 200:
+                res.end("Success! Sent magic packet.");
+                break;
+            case 400:
+                res.end("Invalid ID.");
+                break;
+            case 500:
+                res.end("Internal server error. Check logs!");
+                break;
+            default:
+                res.end("Other status code. Check logs!");
+                break;
+        }
+    } else {
+        res.statusCode = 404; // 404 Not Found
+        res.end("This is a WOL Web server. Send a POST request to /wake instead.");
+    }
+});
+
+server.listen(config["port"], () => log(`Server started running on port ${config["port"]}!`));
